ad7eed85c5155a4c7294cdd44f9d9a68b13bc55e
James Tomasino <james@tomasino.org>
2018-09-25 14:22:12 -0400

    strip spaces

--- parse.py
+++ parse.py
@@ -6,8 +6,7 @@ def chunks(s, n):
         yield s[start:start+n]
 
 with open('code.txt') as txt:
-    # read in code, but strip linebreaks
-    code=txt.read().replace('\n', '').replace('\r', '')
+    # read in code, but strip linebreaks and spaces
+    code=txt.read().replace('\n', '').replace('\r', '').replace(' ', '')
     for chunk in chunks(code, 8):
         print (chr(int(chunk, 2)), end='')
-#bin = [int(i) for element in ["{0:b}".format(ord(c)) for c in message] for i in element]
